44b4c2e8804f1e8a999c75318b002bcd2b34a3ef
celeri <celestin.bouchet@gmail.com>
2025-03-26 10:29:04 +0100

    add example module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppService } from './app.service';
 import { AppRouter } from './app.router';
 import { TRPCModule } from 'nestjs-trpc';
 import { TrpcPanelController } from './trpc-panel.controller';
+import { ExampleModule } from './example/example.module';
 
 @Module({
   imports: [
     TRPCModule.forRoot({
       autoSchemaFile: './src/@generated',
     }),
+    ExampleModule,
   ],
   controllers: [TrpcPanelController],
   providers: [AppService, AppRouter],
