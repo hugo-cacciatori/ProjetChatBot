07c2878547e3613a57e7cfe1cba82d39535ae1a3
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-03-26 17:35:33 +0100

    remove obsolete import from trpc

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TRPCModule } from 'nestjs-trpc';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
