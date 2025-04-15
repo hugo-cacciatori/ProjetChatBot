7f8dec085c0be97a79311e7c94cc0189fc8d6d7d
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-04-15 10:47:33 +0200

    add authGuard to controllers and swagger config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().build();
+  const config = new DocumentBuilder().addBearerAuth().build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
